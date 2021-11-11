523957bb9c291cbfdd077df8d97ba981c462b442
darcy <darcy.u83@gmail.com>
2021-11-11 17:11:38 +0900

    component/props/propTypes setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
 
+function Food({ name, image, rating }) {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <h4>{rating}/5</h4>
+      <img
+        src={image}
+        style={{ width: "200px", height: "200px" }}
+        alter={name}
+      />
+    </div>
+  );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  rating: PropTypes.number,
+};
+
+function renderFood(dish) {
+  console.log(dish);
+  return (
+    <Food
+      key={dish.id}
+      name={dish.name}
+      image={dish.image}
+      rating={dish.rating}
+    />
+  );
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: "kimchi",
+    image:
+      "https://cdn.pixabay.com/photo/2014/01/09/10/14/kimchi-fried-rice-241051_960_720.jpg",
+    rating: 4.3,
+  },
+  {
+    id: 2,
+    name: "bibimbab",
+    image:
+      "https://cdn.pixabay.com/photo/2020/02/28/12/40/bibimbap-4887394_960_720.jpg",
+    rating: 3.9,
+  },
+  {
+    id: 3,
+    name: "jeon",
+    image:
+      "https://cdn.pixabay.com/photo/2016/04/22/18/36/kimchi-pizza-1346285_960_720.jpg",
+    rating: 5.0,
+  },
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      {foodILike.map(renderFood)}
     </div>
   );
 }
