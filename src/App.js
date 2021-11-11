70a4b1777cbc3c2cb2ff31a5172bf0d76cd421ef
darcy <darcy.u83@gmail.com>
2021-11-11 18:27:24 +0900

    life cycle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 class App extends React.Component {
-  state = {
-    count: 0,
-  };
+  constructor(props) {
+    super(props);
+    console.log("Constructor");
+    this.state = { count: 0 };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
 
   add = () => {
     this.setState((current) => ({ count: current.count + 1 }));
@@ -13,9 +27,11 @@ class App extends React.Component {
     this.setState((current) => ({ count: current.count - 1 }));
   };
   render() {
+    console.log("render");
     return (
       <div>
         <h1>The number is {this.state.count}</h1>
+
         <button onClick={this.add}>Add</button>
         <button onClick={this.minus}>Minus</button>
       </div>
