c186ae6591488b7ecdfeaba27c67b5483292393b
darcy <darcy.u83@gmail.com>
2021-11-13 17:33:24 +0900

    readMe changed

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 # Welcome to practices on React old coding style
 
+https://darcyu83.github.io/react-movie-oldstyle/
+
 # author : yuds
 
 # thx to visit
--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
+import axios from "axios";
 import React from "react";
+import Movie from "./components/Movie";
+import "./App.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("Constructor");
-    this.state = { count: 0 };
-  }
+  state = { isLoading: true, movies: [] };
 
   componentDidMount() {
-    console.log("componentDidMount");
+    this.getMovies();
   }
 
-  componentDidUpdate() {
-    console.log("componentDidUpdate");
-  }
-
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-  }
-
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies });
+    this.setState({ isLoading: false });
+    console.log(movies);
   };
 
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
-  render() {
-    console.log("render");
+  renderMovie(props, index) {
+    console.log("index", index);
     return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <Movie
+        key={props.index}
+        id={props.id}
+        year={props.year}
+        title={props.title}
+        summary={props.summary}
+        poster={props.medium_cover_image}
+        genres={props.genres}
+      />
+    );
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Lodaing...</span>
+          </div>
+        ) : (
+          <div className="movies">{movies.map(this.renderMovie)}</div>
+        )}
+      </section>
     );
   }
 }
